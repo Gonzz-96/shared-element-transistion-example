8d71d9666afd51ed98db627504fef1b0d199a863
Gonzalo Campos <erick.campos@wizeline.com>
2020-05-08 21:18:41 -0500

    Create new activity.

---
+++ app/src/main/java/com/gonz/mx/shared/element/example/FullScreenImageActivity.kt
@@ -0,0 +1,12 @@
+package com.gonz.mx.shared.element.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullScreenImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_image)
+    }
+}
