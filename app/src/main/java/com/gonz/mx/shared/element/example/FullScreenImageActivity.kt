ce4f6545bafae1d321403906a1be43997a8efdc6
Gonzalo Campos <erick.campos@wizeline.com>
2020-05-08 21:27:34 -0500

    Prepare second activity layout.

--- app/src/main/java/com/gonz/mx/shared/element/example/FullScreenImageActivity.kt
+++ app/src/main/java/com/gonz/mx/shared/element/example/FullScreenImageActivity.kt
@@ -8,5 +8,7 @@ class FullScreenImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_screen_image)
+
+        setTitle("Activity 2")
     }
 }
